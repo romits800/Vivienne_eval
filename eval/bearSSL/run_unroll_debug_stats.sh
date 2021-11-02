e0e0b411867c9443faad5a39ae26e89c793abdef
Romy Tsoupidi <tsoupidi@kth.se>
2021-11-02 16:44:09 +0100

    Add stats collecting stats for bearSSL.

--- eval/bearSSL/run_unroll_debug_stats.sh
+++ eval/bearSSL/run_unroll_debug_stats.sh
@@ -3,14 +3,19 @@
 ulimit -n 100000
 
 to=$1
-for i in 0 3
+
+for solver in "z3" "z3_bindings" "yices2" "cvc4" "boolector" "bitwuzla" 
 do
-    timeout $to ../../Vivienne/interpreter/wasm imports_aes.wast aes_big_wrapper_O${i}.wast --debug --stats -t -i script_aes_big.wast 
+  for i in 0 3
+  do
+    flags="-t --stats --solver $solver"
+    timeout $to ../../Vivienne/interpreter/wasm imports_aes.wast aes_big_wrapper_O${i}.wast $flags -i script_aes_big.wast 
     echo $?
-    timeout $to ../../Vivienne/interpreter/wasm imports_aes.wast aes_ct_wrapper_O${i}.wast  --debug --stats -t -i script_aes_ct.wast 
+    timeout $to ../../Vivienne/interpreter/wasm imports_aes.wast aes_ct_wrapper_O${i}.wast  $flags -i script_aes_ct.wast 
     echo $?
-    timeout $to ../../Vivienne/interpreter/wasm imports_des.wast des_ct_wrapper_O${i}.wast  --debug --stats -t -i script_des_ct.wast 
+    timeout $to ../../Vivienne/interpreter/wasm imports_des.wast des_ct_wrapper_O${i}.wast  $flags -i script_des_ct.wast 
     echo $?
-    timeout $to ../../Vivienne/interpreter/wasm imports_des.wast des_tab_wrapper_O${i}.wast --debug --stats -t -i script_des_tab.wast 
+    timeout $to ../../Vivienne/interpreter/wasm imports_des.wast des_tab_wrapper_O${i}.wast $flags -i script_des_tab.wast 
     echo $?
+  done
 done
